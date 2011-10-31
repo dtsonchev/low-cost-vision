bf128f988ef21a8b4bd0a170a5b537a80a6b96d3
zep.mouris@gmail.com <zep.mouris@gmail.com@4ab6f9c5-c1da-f840-b2ac-8f7b3113cc1e>
2011-10-31 10:36:09 +0000

    Added a program which can make images from our camera by pressing B. It is posible to change the exposure and the color with A and W,S

---
+++ trunk/Vision/captureImage/captureImage.cpp
@@ -0,0 +1,173 @@
+#include "unicap_cv_bridge.hpp"
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <cstdio>
+#include <sstream>
+
+using namespace cv;
+using namespace std;
+using namespace unicap_cv_bridge;
+
+void draw_hist(Mat& dest, const Mat& src, int histSize)
+{
+	rectangle(dest, Point(0, 0), Point(dest.cols, dest.rows), Scalar(0, 0, 0, 0), CV_FILLED, 1, 0);
+	// Separate the image in 3 places ( R, G and B )
+	vector<Mat> rgb_planes;
+	split(src, rgb_planes);
+
+	// Set the ranges ( for R,G,B) )
+	float range[] = { 0, 255 } ;
+	const float* histRange = { range };
+
+	Mat r_hist, g_hist, b_hist;
+
+	// Compute the histograms:
+	calcHist(&rgb_planes[0], 1, 0, Mat(), r_hist, 1, &histSize, &histRange, true, false);
+	calcHist(&rgb_planes[1], 1, 0, Mat(), g_hist, 1, &histSize, &histRange, true, false);
+	calcHist(&rgb_planes[2], 1, 0, Mat(), b_hist, 1, &histSize, &histRange, true, false);
+
+	// Draw the histograms for R, G and B
+	int hist_w = dest.cols, hist_h = dest.rows;
+	int bin_w = cvRound((double)(hist_w)/histSize);
+
+	float hist_max = 0;
+	for(int i = 0; i < histSize; i++){
+		int x = r_hist.at<float>(i);
+		if(x > hist_max) { hist_max = x; }
+		x = g_hist.at<float>(i);
+		if(x > hist_max) { hist_max = x; }
+		x = b_hist.at<float>(i);
+		if(x > hist_max) { hist_max = x; }
+	}
+
+	r_hist *= (dest.rows / (double)hist_max);
+	g_hist *= (dest.rows / (double)hist_max);
+	b_hist *= (dest.rows / (double)hist_max);
+
+	// Draw for each channel
+	int i;
+	for(i = 1; i < histSize; i++){
+		line(
+			dest,
+			Point(bin_w * (i - 1), hist_h - cvRound(r_hist.at<float>(i - 1))),
+			Point(bin_w * (i), hist_h - cvRound(r_hist.at<float>(i))),
+			Scalar(255, 0, 0), 1, 8, 0);
+		line(
+			dest,
+			Point(bin_w * (i - 1), hist_h - cvRound(g_hist.at<float>(i - 1))),
+			Point(bin_w * (i), hist_h - cvRound(g_hist.at<float>(i))),
+			Scalar(0, 255, 0), 1, 8, 0);
+		line(
+			dest,
+			Point(bin_w * (i - 1), hist_h - cvRound(b_hist.at<float>(i - 1))),
+			Point(bin_w * (i), hist_h - cvRound(b_hist.at<float>(i))),
+			Scalar(0, 0, 255), 1, 8, 0);
+	}
+
+	line(
+		dest,
+		Point(bin_w * (i - 1), hist_h - cvRound(r_hist.at<float>(i - 1))),
+		Point(bin_w * (i), hist_h - cvRound(r_hist.at<float>(i - 1))),
+		Scalar(255, 0, 0), 1, 8, 0);
+	line(
+		dest,
+		Point(bin_w * (i - 1), hist_h - cvRound(g_hist.at<float>(i - 1))),
+		Point(bin_w * (i), hist_h - cvRound(g_hist.at<float>(i - 1))),
+		Scalar(0, 255, 0), 1, 8, 0);
+	line(
+		dest,
+		Point(bin_w * (i - 1), hist_h - cvRound(b_hist.at<float>(i - 1))),
+		Point(bin_w * (i), hist_h - cvRound(b_hist.at<float>(i - 1))),
+		Scalar(0, 0, 255), 1, 8, 0);
+}
+
+int main(int argc, char** argv)
+{
+	if(argc == 1){
+		printf("usage for starting capture: %s device_number format_number\ndevices:\n", argv[0]);
+		print_devices_inventory();
+
+		return 0;
+	}else if (argc != 3){
+		return 1;
+	}
+
+	int device_number = atoi(argv[1]);
+	int format_number = atoi(argv[2]);
+
+	unicap_cv_camera cam(device_number, format_number);
+
+	Mat frame(cam.get_img_height(), cam.get_img_width(), cam.get_img_format());
+	Mat hist(600, 900, CV_8UC3);
+
+	cam.set_white_balance(1, 1);
+
+	Mat cameraMatrix, distCoeffs;
+	Mat undistorted, map1, map2, rotated;
+
+	FileStorage fs("cameraMatrix.xml", FileStorage::READ);
+	fs["cameraMatrix"] >> cameraMatrix;
+	fs["distCoeffs"] >> distCoeffs;
+
+	initUndistortRectifyMap(cameraMatrix, distCoeffs, cv::Mat(), cv::Mat(), frame.size(), CV_32FC1, map1, map2);
+
+	int counter = 0;
+	double exposure = 0.03;
+	double blue, red;
+	char key = 0;
+	bool tripmode = false;
+
+	while(key != 'q'){
+
+		cam.get_frame(&frame);
+		remap(frame, undistorted, map1, map2, INTER_LINEAR);
+
+		transpose(undistorted, rotated);
+		flip(rotated, rotated, -1);
+		
+		draw_hist(hist, frame, 128);
+
+		cam.get_white_balance(blue, red);
+
+		rectangle(hist, Point(10, 5), Point(210, 67), Scalar(100, 100, 100, 50), CV_FILLED, 1, 0);
+
+		stringstream ss;
+		ss << "exposure: " << exposure;
+		cv::putText(hist, ss.str(), cv::Point(20,20), FONT_HERSHEY_PLAIN, 1, cv::Scalar(255, 255, 255, 0), 1, 1, false);
+		ss.str("");
+		ss << "blue: " << blue;
+		cv::putText(hist, ss.str(), cv::Point(20, 40), FONT_HERSHEY_PLAIN, 1, cv::Scalar(255, 255, 255, 0), 1, 1, false);
+		ss.str("");
+		ss << "red:  " << red;
+		cv::putText(hist, ss.str(), cv::Point(20, 60), FONT_HERSHEY_PLAIN, 1, cv::Scalar(255, 255, 255, 0), 1, 1, false);
+
+		imshow("histogram", hist);
+		imshow("rotated", rotated);
+
+		key = waitKey(10);
+		if(key == 'w'){
+			exposure *= 1.125;
+			cam.set_exposure(exposure);
+		}else if(key == 's'){
+			exposure /= 1.125;
+			cam.set_exposure(exposure);
+		}else if(key == 'a'){
+			cam.set_auto_white_balance(true);
+		}else if(key == 'z'){
+			cam.set_auto_white_balance(false);
+		}else if(key == 't'){
+			tripmode = !tripmode;
+			cam.set_trip_mode(tripmode);
+		}else if(key == 'b'){
+			ss.str("");
+			ss << "Image" << counter++ << ".jpg";
+			imwrite(ss.str().c_str(), rotated);
+			cout << "Image taken: " << ss.str().c_str() << endl;
+			key = 0;	
+		}
+		cout.flush();
+	}
+
+	return 0;
+}
---
+++ trunk/Vision/captureImage/unicap_cv_bridge.hpp
@@ -0,0 +1,150 @@
+#pragma once
+
+extern "C"
+{
+    #define private stupid_unicap_library_that_uses_cpp_keywords
+    #include <unicap.h>
+    #include <unicap_status.h>
+    #undef private
+};
+
+#include <boost/thread.hpp>
+#include <opencv2/core/core.hpp>
+#include <stdexcept>
+#include <string>
+#include <vector>
+
+namespace unicap_cv_bridge
+{
+	/**
+	 * @brief obtain a list of device names
+	 * @param devices output parameter. will contain device names
+	 **/
+	void list_devices(std::vector<std::string>& devices);
+	
+	/**
+	 * @brief obtain a list of formats
+	 * @param dev the device number
+	 * @param formats output parameter. will contain format desciptions
+	 **/
+	void list_formats(int dev, std::vector<std::string>& formats);
+
+	/**
+	 * @brief obtain a list of properties
+	 * @param dev the device number
+	 * @param properties output parameter. will contain properties
+	 **/
+	void list_properties(int dev, std::vector<std::string>& properties);
+	/**
+	* @brief print a list of all connected devices with it's formats
+	* @param out the output stream. Defaults to standard out
+	**/
+	void print_devices_inventory(std::ostream& out = std::cout);
+
+	/**
+	 * @brief unicap exception
+	 **/
+	class unicap_cv_exception : public std::runtime_error
+	{
+		public:
+			unicap_cv_exception(const std::string& msg) : std::runtime_error(msg) {}
+			virtual ~unicap_cv_exception(void) throw() {}
+	};
+
+	/**
+	 * @brief handle to unicap camera
+	 **/
+	class unicap_cv_camera
+	{
+		private:
+			typedef enum  _frame_cap_state
+			{
+				FCSTATE_DONT_COPY,
+				FCSTATE_COPY,
+				FCSTATE_SUCCESS,
+				FCSTATE_FAILURE
+			} frame_cap_state;
+		
+			bool tripmode;
+
+			unicap_handle_t handle;
+			unicap_device_t device;
+			unicap_format_t format;
+        
+			boost::mutex mut;
+			boost::condition_variable cond;
+			frame_cap_state state;
+			cv::Mat* mat;
+        
+		public:
+			/**
+			 * @brief constructs a unicap camera
+			 * @param dev device number
+			 * @param format format number
+			 **/
+			unicap_cv_camera(int dev, int format);
+
+			~unicap_cv_camera(void);
+
+			void set_trip_mode(bool tripmode);
+
+			/**
+			 * @brief get white balance settings
+			 * @param blue out parameter
+			 * @param red out parameter
+			 **/
+			void get_white_balance(double& blue, double& red);
+
+			/**
+			 * @brief set white balance settings
+			 * @param blue blue
+			 * @param red red
+			 **/
+			void set_white_balance(double blue, double red);
+
+			/**
+			 * @brief set exposure
+			 * @param exposure exposure
+			 **/
+			void set_exposure(double exposure);
+
+			/**
+			 * @brief set auto white balance
+			 * @param automatic true for on, false otherwise
+			 **/
+			void set_auto_white_balance(bool automatic);
+
+			/**
+			 * @brief get outputed image width
+			 * @return image width
+			 **/
+			int get_img_width(void);
+
+			/**
+			 * @brief get outputed image height
+			 * @return image height
+			 **/
+			int get_img_height(void);
+
+			/**
+			 * @brief get outputed image format
+			 * @return image format
+			 **/
+			int get_img_format(void);
+
+			/**
+			 * @brief callback function which gets called when a new frame is captured
+			 * @note should not be called by user
+			 * @param buffer buffer which holds the frame
+			 **/        
+			void new_frame_cb(unicap_data_buffer_t* buffer);
+
+			/**
+			 * @brief get a frame
+			 * 
+			 * this function blocks until a new frame is captured
+			 * @param mat the frame is copied into this matrix
+			 **/        
+			void get_frame(cv::Mat* mat);
+	};
+}
