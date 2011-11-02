565a85f2cb99dcd84688a11068e36395469f814d
francpape <francpape@4ab6f9c5-c1da-f840-b2ac-8f7b3113cc1e>
2011-11-02 16:03:00 +0000

    Subclipse test

---
+++ trunk/MAST/MyProject/src/main.cpp
@@ -0,0 +1,19 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Nov 2, 2011
+ *      Author: franc
+ */
+
+#include <stdio.h>
+
+/**
+* @brief "SHORT DESCRIPTION OF THE FUNCTION"
+* @details "DETAILS OF THE FUNCTION"
+* @return it returns an example
+*/
+int main()
+{
+	printf("Hello World\n");
+	return 0;
+}
