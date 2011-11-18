6880ce7184b74187d27ac8dc44def8524506be5c
zep.mouris <zep.mouris@gmail.com>
2011-11-18 11:09:48 +0000

    Add the first version of a program which take 2 images or 2 camera's and calculate a depth images.

---
+++ Vision/StereoVisionDepth/src/stereovisiondepth.cpp
@@ -0,0 +1,246 @@
+/*
+ * stereovisiondepth.cpp
+ *
+ *  Created on: Nov 17, 2011
+ *      Author: zep
+ */
+
+#include "stereovisiondepth.h"
+#include <opencv2/calib3d/calib3d.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/contrib/contrib.hpp>
+#include <opencv2/core/core.hpp>
+#include <iostream>
+#include <stdio.h>
+
+using namespace cv;
+using namespace std;
+
+void updateBM(int, void* stereovisiondepthobject){
+	stereovisiondepth* temp = (stereovisiondepth*) stereovisiondepthobject;
+
+	temp->bm.state->preFilterCap = ((temp->bm_preFilterCapValue <= 0 ? 1 : temp->bm_preFilterCapValue)|1) ;
+	temp->bm.state->SADWindowSize = ((temp->bm_SADWindowSize <= 4 ? 5 : temp->bm_SADWindowSize)|1);
+	temp->bm.state->minDisparity = (temp->bm_minDisparity/16)*16;
+	temp->bm.state->numberOfDisparities = ((temp->bm_numberOfDisparities <= 15 ? 16 : temp->bm_numberOfDisparities)/16)*16;
+	temp->bm.state->textureThreshold = temp->bm_textureThreshold;
+	temp->bm.state->uniquenessRatio = temp->bm_uniquenessRatio;
+	temp->bm.state->speckleWindowSize = temp->bm_speckleWindowSize;
+	temp->bm.state->speckleRange = temp->bm_speckleRange;
+	temp->bm.state->disp12MaxDiff = temp->bm_disp12MaxDiff;
+}
+
+void updateSGBM(int, void* stereovisiondepthobject){
+	stereovisiondepth* temp = (stereovisiondepth*) stereovisiondepthobject;
+	temp->sgbm.preFilterCap = temp->sgbm_preFilterCap;
+	temp->sgbm.SADWindowSize = ((temp->sgbm_SADWindowSize <= 0 ? 1 : temp->sgbm_SADWindowSize)|1);
+
+	int cn = temp->imgL.channels();
+
+	temp->sgbm.P1 = 8 * cn * temp->sgbm.SADWindowSize * temp->sgbm.SADWindowSize;
+	temp->sgbm.P2 = 32 * cn * temp->sgbm.SADWindowSize * temp->sgbm.SADWindowSize;
+	temp->sgbm.minDisparity = temp->sgbm_minDisparity;
+	temp->sgbm.numberOfDisparities =  ((temp->sgbm_numberOfDisparities <= 15 ? 16 : temp->sgbm_numberOfDisparities)/16)*16;
+	temp->sgbm.uniquenessRatio = temp->sgbm_uniquenessRatio;
+	temp->sgbm.speckleWindowSize = temp->sgbm_speckleWindowSize;
+	temp->sgbm.speckleRange = (temp->sgbm_speckleRange / 16) * 16;
+	temp->sgbm.disp12MaxDiff = temp->sgbm_disp12MaxDiff;
+}
+
+void updateHH(int, void* stereovisiondepthobject){
+	stereovisiondepth* temp = (stereovisiondepth*) stereovisiondepthobject;
+	temp->hh.preFilterCap = temp->hh_preFilterCap;
+	temp->hh.SADWindowSize = ((temp->hh_SADWindowSize <= 0 ? 1 : temp->hh_SADWindowSize)|1);
+
+	int cn = temp->imgL.channels();
+
+	temp->hh.P1 = 8 * cn * temp->hh.SADWindowSize * temp->hh.SADWindowSize;
+	temp->hh.P2 = 32 * cn * temp->hh.SADWindowSize * temp->hh.SADWindowSize;
+	temp->hh.minDisparity = temp->hh_minDisparity;
+	temp->hh.numberOfDisparities =  ((temp->hh_numberOfDisparities <= 15 ? 16 : temp->hh_numberOfDisparities) / 16) * 16;
+	temp->hh.uniquenessRatio = temp->hh_uniquenessRatio;
+	temp->hh.speckleWindowSize = temp->hh_speckleWindowSize;
+	temp->hh.speckleRange = (temp->hh_speckleRange / 16) * 16;
+	temp->hh.disp12MaxDiff = temp->hh_disp12MaxDiff;
+}
+
+stereovisiondepth::stereovisiondepth(Mat imgL, Mat imgR) {
+	this->imgL = imgL;
+	this->imgR =imgR;
+
+
+	resultBM = imgL.clone();
+	resultSGBM = imgL.clone();
+	resultHH = imgL.clone();
+	resultVAR = imgL.clone();;
+
+	imshow("bm", resultBM);
+	imshow("sgbm", resultSGBM);
+	imshow("hh", resultHH);
+	imshow("var", resultVAR);
+
+	initVar();
+	initTrackBars();
+}
+
+stereovisiondepth::stereovisiondepth(Mat imgL, Mat imgR, Rect roiL, Rect roiR) {
+	this->imgL = imgL;
+	this->imgR =imgR;
+
+	bm.state->roi1 = roiL;
+	bm.state->roi2 = roiR;
+
+	resultBM = imgL.clone();
+	resultSGBM = imgL.clone();
+	resultHH = imgL.clone();
+	resultVAR = imgL.clone();;
+
+	imshow("bm", resultBM);
+	imshow("sgbm", resultSGBM);
+	imshow("hh", resultHH);
+	imshow("var", resultVAR);
+
+	initVar();
+	initTrackBars();
+}
+
+void stereovisiondepth::initTrackBars(){
+	createTrackbar( "preFilterCap", "bm", &bm_preFilterCapValue, 63, updateBM, this );
+	 createTrackbar( "SADWindowSize", "bm", &bm_SADWindowSize, 33 , updateBM, this);
+	 createTrackbar( "minDisparity", "bm", &bm_minDisparity, 30 ,updateBM, this);
+	 createTrackbar( "numberOfDisparities", "bm", &bm_numberOfDisparities, 255 ,updateBM, this);
+	 createTrackbar( "textureThreshold", "bm", &bm_textureThreshold, 255,updateBM, this );
+	 createTrackbar( "uniquenessRatio", "bm", &bm_uniquenessRatio, 30, updateBM, this);
+	 createTrackbar( "speckleWindowSize", "bm", &bm_speckleWindowSize, 500 ,updateBM, this);
+	 createTrackbar( "speckleRange", "bm", &bm_speckleRange, 10000,updateBM, this );
+	 createTrackbar( "disp12MaxDiff", "bm", &bm_disp12MaxDiff, 10000 ,updateBM , this);
+
+	 createTrackbar( "preFilterCap", "sgbm", &sgbm_preFilterCap, 255, updateSGBM , this);
+	 createTrackbar( "SADWindowSize", "sgbm", &sgbm_SADWindowSize, 33, updateSGBM , this);
+	 createTrackbar( "minDisparity", "sgbm", &sgbm_minDisparity, 30, updateSGBM , this);
+	 createTrackbar( "numberOfDisparities", "sgbm", &sgbm_numberOfDisparities, 255, updateSGBM , this);
+	 createTrackbar( "uniquenessRatio", "sgbm", &sgbm_uniquenessRatio, 50, updateSGBM , this);
+	 createTrackbar( "speckleWindowSize", "sgbm", &sgbm_speckleWindowSize, 300, updateSGBM , this);
+	 createTrackbar( "speckleRange", "sgbm", &sgbm_speckleRange, 2555, updateSGBM , this);
+	 createTrackbar( "disp12MaxDiff", "sgbm", &sgbm_disp12MaxDiff, 50 ,updateSGBM , this);
+
+	 createTrackbar( "preFilterCap", "hh", &hh_preFilterCap, 255, updateHH , this);
+	 createTrackbar( "SADWindowSize", "hh", &hh_SADWindowSize, 33, updateHH , this);
+	 createTrackbar( "minDisparity", "hh", &hh_minDisparity, 30, updateHH , this);
+	 createTrackbar( "numberOfDisparities", "hh", &hh_numberOfDisparities, 255, updateHH , this);
+	 createTrackbar( "uniquenessRatio", "hh", &hh_uniquenessRatio, 50, updateHH , this);
+	 createTrackbar( "speckleWindowSize", "hh", &hh_speckleWindowSize, 300, updateHH , this);
+	 createTrackbar( "speckleRange", "hh", &hh_speckleRange, 255, updateHH , this);
+	 createTrackbar( "disp12MaxDiff", "hh", &hh_disp12MaxDiff, 50 ,updateHH , this);
+}
+void stereovisiondepth::initVar(){
+	 //init on standaard values
+	 bm_preFilterCapValue = 0;
+	 bm_SADWindowSize = 0;
+	 bm_minDisparity = 0;
+	 bm_numberOfDisparities = 0;
+	 bm_textureThreshold = 0;
+	 bm_uniquenessRatio = 0;
+	 bm_speckleWindowSize = 0;
+	 bm_speckleRange = 0;
+	 bm_disp12MaxDiff = 0;
+
+	 sgbm_preFilterCap = 0;
+	 sgbm_SADWindowSize = 0;
+	 sgbm_minDisparity = 0;
+	 sgbm_numberOfDisparities = 0;
+	 sgbm_uniquenessRatio = 0;
+	 sgbm_speckleWindowSize = 0;
+	 sgbm_speckleRange = 0;
+	 sgbm_disp12MaxDiff = 0;
+
+	 hh_preFilterCap = 0;
+	 hh_SADWindowSize = 0;
+	 hh_minDisparity = 0;
+	 hh_numberOfDisparities = 0;
+	 hh_uniquenessRatio = 0;
+	 hh_speckleWindowSize = 0;
+	 hh_speckleRange = 0;
+	 hh_disp12MaxDiff = 0;
+
+	 var_levels = 0;							// ignored with USE_AUTO_PARAMS
+	 var_pyrScale = 0.;							// ignored with USE_AUTO_PARAMS
+	 var_nIt = 0;
+	 var_minDisp = 0;
+	 var_maxDisp = 0;
+	 var_poly_n = 0;
+	 var_poly_sigma = 0.0;
+	 var_fi = 0.0;
+	 var_lambda = 0.0;
+	 var_penalization = var.PENALIZATION_TICHONOV;	// ignored with USE_AUTO_PARAMS
+	 var_cycle = var.CYCLE_V;						// ignored with USE_AUTO_PARAMS
+	 var_flags = 0;
+}
+
+void stereovisiondepth::DoBM(){
+	Mat imgLL , imgRR,temp;
+	cvtColor(imgL,imgLL, CV_RGB2GRAY);
+	cvtColor(imgR, imgRR, CV_RGB2GRAY);
+	bm(imgLL, imgRR, temp);
+	temp.convertTo(resultBM, CV_8U, 255 / (bm.state->numberOfDisparities * 16.));
+}
+
+void stereovisiondepth::DoSGBM(){
+	Mat temp;
+	sgbm.fullDP = 0;
+	sgbm.disp12MaxDiff = 1;
+	sgbm.numberOfDisparities = (sgbm.numberOfDisparities == 0 ? 16 : sgbm.numberOfDisparities);
+	sgbm(imgL, imgR, temp);
+	temp.convertTo(resultSGBM, CV_8U, 255 / (sgbm.numberOfDisparities * 16.));
+}
+
+void stereovisiondepth::DoHH(){
+	Mat temp;
+	hh.fullDP = 1;
+	hh.disp12MaxDiff = 1;
+	hh.numberOfDisparities = (hh.numberOfDisparities == 0 ? 16 : hh.numberOfDisparities);
+	hh(imgL, imgR, temp);
+	temp.convertTo(resultHH, CV_8U, 255 / (hh.numberOfDisparities * 16.));
+}
+
+void stereovisiondepth::DoVAR(){
+	Mat temp;
+	var(imgL, imgR, temp);
+	temp.convertTo(resultVAR, CV_8U);
+}
+
+void stereovisiondepth::updateImages(Mat imgL, Mat imgR){
+	this->imgL = imgL;
+	this->imgR = imgR;
+}
+
+void stereovisiondepth::show(){
+	imshow("bm", resultBM);
+	imshow("sgbm", resultSGBM);
+	imshow("hh", resultHH);
+	imshow("var", resultVAR);
+}
+
+void stereovisiondepth::saveXYZ(const char* filename)
+{
+	Mat matList[] = {resultBM, resultSGBM,resultHH,resultVAR};
+
+	const double max_z = 1.0e4;
+	FILE* fp = fopen(filename, "wt");
+
+	for(int i = 0; i < 4; i++){
+		for(int y = 0; y < matList[i].rows; y++){
+			for(int x = 0; x < matList[i].cols; x++){
+
+				Vec3f point = matList[i].at<Vec3f>(y, x);
+				if(fabs(point[2] - max_z) < FLT_EPSILON || fabs(point[2]) > max_z){
+					continue;
+				}
+				fprintf(fp, "%f %f %f\n", point[0], point[1], point[2]);
+			}
+		}
+		fprintf(fp, "\n\n\n\n");
+    }
+    fclose(fp);
+}
---
+++ Vision/StereoVisionDepth/src/stereovisiondepth.h
@@ -0,0 +1,216 @@
+/*
+ * stereovisiondepth.h
+ *
+ *  Created on: Nov 17, 2011
+ *      Author: zep
+ */
+
+#ifndef STEREOVISIONDEPTH_H_
+#define STEREOVISIONDEPTH_H_
+
+#include <opencv2/calib3d/calib3d.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/contrib/contrib.hpp>
+
+/**
+ * @brief thic class creates 4 different depth images each with a different algorithm.
+ */
+class stereovisiondepth {
+public:
+	/**
+	 *Useless constructor
+	 */
+	stereovisiondepth(){}
+	/**
+	 *Constructor used when no undistortion xml is used.
+	 *@param imgL left image
+	 *@param imgR right image
+	 */
+	stereovisiondepth(cv::Mat imgL, cv::Mat imgR);
+	/**
+	 *Constructor used when undistortion xml is used.
+	 *@param imgL left image
+	 *@param imgR right image
+	 *@param roiL the region of interest of the left image.
+	 *@param roiR the region of interest of the right image.
+	 */
+	stereovisiondepth(cv::Mat imgL, cv::Mat imgR,cv::Rect roiL,cv::Rect roiR);
+	/**
+	 * Destructor
+	 */
+	virtual ~stereovisiondepth(){}
+	/**
+	 * This function does the BM algorithm.
+	 */
+	void DoBM();
+	/**
+	 * This function does the SGBM algorithm.
+	 */
+	void DoSGBM();
+	/**
+	 * This function does the HH algorithm.
+	 */
+	void DoHH();
+	/**
+	 * This function does the VAR algorithm (currently no documentation at the opencv library)
+	 */
+	void DoVAR();
+	/**
+	 * updates the images used by the depth algorithms
+	 * @param imgL The left images.
+	 * @param imgR The right images.
+	 */
+	void updateImages(cv::Mat imgL, cv::Mat imgR);
+	/**
+	 * function which shows the result of the algorithms.
+	 */
+	void show();
+	/**
+	 * function for storing all the pixels separately.
+	 * @param filename The path and name for the file.
+	 */
+	void saveXYZ(const char* filename);
+
+	///BM algorithm implementation object.
+	cv::StereoBM bm;
+	///SGBM algorithm implementation object.
+	cv::StereoSGBM sgbm;
+	///HH algorithm implementation object.
+	cv::StereoSGBM hh;
+	///VAR algorithm implementation object, currently no documentation at opencv.
+	cv::StereoVar var;
+
+	///The left image used for all the algorithms
+	cv::Mat imgL;
+	///The right image used for all the algorithms
+	cv::Mat imgR;
+
+	///The result image with the results of all the algorithm
+	cv::Mat resultBM;
+	///The result image with the results of all the algorithm
+	cv::Mat resultSGBM;
+	///The result image with the results of all the algorithm
+	cv::Mat resultHH;
+	///The result image with the results of all the algorithm
+	cv::Mat resultVAR;
+
+	///a parameter of the BM algorithm.
+	int bm_preFilterCapValue;
+	///a parameter of the BM algorithm.
+	int bm_SADWindowSize;
+	///a parameter of the BM algorithm.
+	int bm_minDisparity;
+	///a parameter of the BM algorithm.
+	int bm_numberOfDisparities;
+	///a parameter of the BM algorithm.
+	int bm_textureThreshold;
+	///a parameter of the BM algorithm.
+	int bm_uniquenessRatio;
+	///a parameter of the BM algorithm.
+	int bm_speckleWindowSize;
+	///a parameter of the BM algorithm.
+	int bm_speckleRange;
+	///a parameter of the BM algorithm.
+	int bm_disp12MaxDiff;
+
+
+	/**A parameter of the SGBM algorithm.
+	 * Truncation value for the prefiltered image pixels. The algorithm first computes x-derivative at each pixel and clips its value by [-preFilterCap, preFilterCap] interval. The result values are passed to the Birchfield-Tomasi pixel cost function.
+	 */
+	int sgbm_preFilterCap;
+	/**A parameter of the SGBM algorithm.
+	 * – Matched block size. It must be an odd number >=1 . Normally, it should be somewhere in the 3..11 range.
+	 */
+	int sgbm_SADWindowSize;
+	/**A parameter of the SGBM algorithm.
+	 * Minimum possible disparity value. Normally, it is zero but sometimes rectification algorithms can shift images, so this parameter needs to be adjusted accordingly.
+	 */
+	int sgbm_minDisparity;
+	/**A parameter of the SGBM algorithm.
+	 * Maximum disparity minus minimum disparity. The value is always greater than zero. In the current implementation, this parameter must be divisible by 16.
+	 */
+	int sgbm_numberOfDisparities;
+	/**A parameter of the SGBM algorithm.
+	 * Margin in percentage by which the best (minimum) computed cost function value should “win” the second best value to consider the found match correct. Normally, a value within the 5-15 range is good enough.
+	 */
+	int sgbm_uniquenessRatio;
+	/**A parameter of the SGBM algorithm.
+	 * Maximum size of smooth disparity regions to consider their noise speckles and invalidate. Set it to 0 to disable speckle filtering. Otherwise, set it somewhere in the 50-200 range.
+	 */
+	int sgbm_speckleWindowSize;
+	/**A parameter of the SGBM algorithm.
+	 * Maximum disparity variation within each connected component. If you do speckle filtering, set the parameter to a positive value, multiple of 16. Normally, 16 or 32 is good enough.
+	 */
+	int sgbm_speckleRange;
+	/**A parameter of the SGBM algorithm.
+	 * Maximum allowed difference (in integer pixel units) in the left-right disparity check. Set it to a non-positive value to disable the check.
+	 */
+	int sgbm_disp12MaxDiff;
+
+	/**A parameter of the HH algorithm.
+	 * Truncation value for the prefiltered image pixels. The algorithm first computes x-derivative at each pixel and clips its value by [-preFilterCap, preFilterCap] interval. The result values are passed to the Birchfield-Tomasi pixel cost function.
+	 */
+	int hh_preFilterCap;
+	/**A parameter of the HH algorithm.
+	 * – Matched block size. It must be an odd number >=1 . Normally, it should be somewhere in the 3..11 range.
+	 */
+	int hh_SADWindowSize;
+	/**A parameter of the HH algorithm.
+	 * Minimum possible disparity value. Normally, it is zero but sometimes rectification algorithms can shift images, so this parameter needs to be adjusted accordingly.
+	 */
+	int hh_minDisparity;
+	/**A parameter of the HH algorithm.
+	 * Maximum disparity minus minimum disparity. The value is always greater than zero. In the current implementation, this parameter must be divisible by 16.
+	 */
+	int hh_numberOfDisparities;
+	/**A parameter of the HH algorithm.
+	 * Margin in percentage by which the best (minimum) computed cost function value should “win” the second best value to consider the found match correct. Normally, a value within the 5-15 range is good enough.
+	 */
+	int hh_uniquenessRatio;
+	/**A parameter of the HH algorithm.
+	 * Maximum size of smooth disparity regions to consider their noise speckles and invalidate. Set it to 0 to disable speckle filtering. Otherwise, set it somewhere in the 50-200 range.
+	 */
+	int hh_speckleWindowSize;
+	/**A parameter of the HH algorithm.
+	 * Maximum disparity variation within each connected component. If you do speckle filtering, set the parameter to a positive value, multiple of 16. Normally, 16 or 32 is good enough.
+	 */
+	int hh_speckleRange;
+	/**A parameter of the HH algorithm.
+	 * Maximum allowed difference (in integer pixel units) in the left-right disparity check. Set it to a non-positive value to disable the check.
+	 */
+	int hh_disp12MaxDiff;
+
+	///A parameter of the VAR algorithm.(currently no opencv documentation).
+	int var_levels;
+	///A parameter of the VAR algorithm.(currently no opencv documentation).
+	int var_pyrScale ;
+	///A parameter of the VAR algorithm.(currently no opencv documentation).
+	int var_nIt ;
+	///A parameter of the VAR algorithm.(currently no opencv documentation).
+	int var_minDisp ;
+	///A parameter of the VAR algorithm.(currently no opencv documentation).
+	int var_maxDisp ;
+	///A parameter of the VAR algorithm.(currently no opencv documentation).
+	int var_poly_n ;
+	///A parameter of the VAR algorithm.(currently no opencv documentation).
+	int var_poly_sigma ;
+	///A parameter of the VAR algorithm.(currently no opencv documentation).
+	int var_fi ;
+	///A parameter of the VAR algorithm.(currently no opencv documentation).
+	int var_lambda ;
+	///A parameter of the VAR algorithm.(currently no opencv documentation).
+	int var_penalization ;
+	///A parameter of the VAR algorithm.(currently no opencv documentation).
+	int var_cycle ;
+	///A parameter of the VAR algorithm.(currently no opencv documentation).
+	int var_flags ;
+	///A parameter of the VAR algorithm.(currently no opencv documentation).
+
+private:
+
+	void initVar();
+	void initTrackBars();
+};
+
+#endif /* STEREOVISIONDEPTH_H_ */
