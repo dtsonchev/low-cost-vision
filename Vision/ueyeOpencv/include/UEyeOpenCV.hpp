7065783d33b8ff008512269b6ff772cecb9eca45
wouter.langerak <Wouter.Langerak@gmail.com>
2011-12-02 11:01:17 +0000

    UEyeOpencv: *	minor fixes

--- Vision/ueyeOpencv/include/UEyeOpenCV.hpp
+++ Vision/ueyeOpencv/include/UEyeOpenCV.hpp
@@ -4,18 +4,19 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <UEyeOpenCVException.hpp>
-//TODO: create function to enable/disable auto-gain
 class UeyeOpencvCam {
 public:
-	UeyeOpencvCam();
-	HIDS getHIDS();
-	void stopCam();
-	~UeyeOpencvCam();
-	/// @deprecated use getFrame(cv::Mat&):void
-	cv::Mat getFrame();
-	void getFrame(cv::Mat& mat);
-	void setAutoWhiteBalance(bool set=true);
+        UeyeOpencvCam(int wdth, int heigh);
+        HIDS getHIDS();
+        void stopCam();
+        ~UeyeOpencvCam();
+        cv::Mat getFrame();
+        void getFrame(cv::Mat& mat);
+        void setAutoWhiteBalance(bool set=true);
+        void setAutoGain(bool set=true);
 private:
-	HIDS hCam;
-	cv::Mat mattie;
+        HIDS hCam;
+        cv::Mat mattie;
+        int width;
+        int height;
 };
