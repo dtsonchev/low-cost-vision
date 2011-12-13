4ae056acbd4afc774a65ac0a648e94acc3e493e5
jules.blok@gmail.com <jules.blok@gmail.com@4ab6f9c5-c1da-f840-b2ac-8f7b3113cc1e>
2011-12-13 11:41:36 +0000

    Share project "Fiducial_test" into "https://low-cost-vision.googlecode.com/svn/trunk/Vision/Fiducial_test"

---
+++ trunk/Vision/Fiducial_test/Fiducial_test/src/main.cpp
@@ -0,0 +1,155 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Dec 6, 2011
+ *      Author: Jules Blok
+ */
+
+#include <string>
+#include <iostream>
+#include <iomanip>
+#include <set>
+#include <map>
+#include <boost/foreach.hpp>
+#include <imageMetaData/Types.hpp>
+#include <imageMetaData/Tools.hpp>
+#include <report/Report.hpp>
+#include <report/CategoryOverview.hpp>
+#include <report/ReportHistogram.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include "FiducialDetector.h"
+#include "CrateDetector.h"
+#include "Crate.h"
+
+
+#ifdef __CDT_PARSER__
+#define foreach(a, b) for(a : b)
+#else
+#define foreach(a, b) BOOST_FOREACH(a, b)
+#endif
+
+#define RESULT_TYPE double
+#define MAX_DEVIATION 0.1
+#define MAX_RANGE 10
+
+using namespace std;
+using namespace imageMetaData;
+using namespace report;
+
+inline float dist(cv::Point2f p1, cv::Point2f p2) { return(sqrt((p1.x-p2.x)*(p1.x-p2.x)+(p1.y-p2.y)*(p1.y-p2.y))); }
+
+int main(int argc, char** argv){
+        if (argc < 3) {
+                cout << "Usage: main <xml path> <xml root tag>\n";
+                return 1;
+        }
+
+        cout << setiosflags(ios::left) << setiosflags(ios::fixed);
+
+        // Load the metadata of the images from an XML file
+        vector<ImageMD> images = getMetaData(argv[1], argv[2]);
+
+        // Create a container for storing the result per image.
+        // Here we use a double as result, but this can be anything
+        map<string, RESULT_TYPE> imgResults;
+        // Create a container for storing the categories and their results
+        CategoriesResults catsResults;
+
+        // The vision algorithm
+        FiducialDetector fidDetector;
+        CrateDetector crateDetector;
+		float totalDistance = 0;
+
+        // Loop through all the images
+        foreach(ImageMD& img, images) {
+                bool success = true;
+
+                cv::Mat image = cv::imread(img.path, CV_LOAD_IMAGE_GRAYSCALE);
+                if(!image.data) {
+                	cout << "Image " << img.path << " not found! Aborting test..." << endl;
+                	return 1;
+                }
+
+                vector<cv::Point2f> points;
+                vector<Crate> crates;
+                fidDetector.detect(image, points);
+                crateDetector.detect(image, crates, points);
+
+                // Check if all crates were detected
+                if(crates.size() != img.objects.size()) {
+                	success = false;
+                }
+
+				foreach(Properties object, img.objects) {
+					cv::Point2f ptTarget((int)object["x"], (int)object["y"]);
+					float lastDistance = dist(crates[0].rect().center,ptTarget);
+					Crate result = crates[0];
+
+					// Get the crate closest to the target
+					foreach(Crate crate, crates) {
+						float d = dist(crate.rect().center,ptTarget);
+						if(d < lastDistance) {
+							result = crate;
+							lastDistance = d;
+						}
+					}
+
+					vector<cv::Point2f> points = result.points();
+					vector<cv::Point2f> targetPoints;
+					targetPoints.push_back(cv::Point2f((int)object["fid1_x"], (int)object["fid1_y"]));
+					targetPoints.push_back(cv::Point2f((int)object["fid2_x"], (int)object["fid2_y"]));
+					targetPoints.push_back(cv::Point2f((int)object["fid3_x"], (int)object["fid3_y"]));
+
+					for(int i=0; i<3; i++) {
+						float d = dist(points[i], targetPoints[i]);
+						if(d > MAX_DEVIATION) {
+							success = false;
+						}
+						totalDistance += d;
+					}
+                }
+
+				imgResults[img.path] = totalDistance / (3 * crates.size());
+
+                // Store the results in the categories container
+                foreach(Category c, img.categories) {
+                        if(success){
+                                // Increment number of correct images in category
+                                catsResults[c.first][c.second].first++;
+                        }
+                        // Increment total number images in category
+                        catsResults[c.first][c.second].second++;
+                }
+        }
+
+		//========================================================================
+		// Create the report
+		//========================================================================
+        Report r("FiducialDetector report");
+
+        // Put the result of each image in a ReportList
+        ReportList* allResults = new ReportList("Fiducial results", 2, STRING, DOUBLE);
+        typedef pair<string, RESULT_TYPE> imgResult;
+        foreach(imgResult imres, imgResults) {
+                allResults->appendRow(imres.first, imres.second);
+        }
+        allResults->setColumnNames("Image path", "Result");
+        r.addField(allResults);
+
+        // Create a histogram of the results
+        ReportHistogram* his = new ReportHistogram("Histogram", getAllValues(imgResults), 10, 10);
+        his->setColumnNames("Range", "Correct images");
+        r.addField(his);
+
+        // Add all categories and their results to the main report
+        foreach(CategoryResults cr, catsResults) {
+                CategoryOverview* cat = new CategoryOverview(cr);
+                cat->setColumnNames("Category", "Correct images", "Percentage correct");
+                r.addField(cat);
+        }
+
+        r.saveHTML("results.html");
+
+        return 0;
+}
+
