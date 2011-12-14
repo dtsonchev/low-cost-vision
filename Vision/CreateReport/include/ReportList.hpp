25183e5b6416d38b52793c5c27390463de5eec95
wouter.langerak <Wouter.Langerak@gmail.com>
2011-12-14 15:18:43 +0000

    CreateReport    *    Added File information    *   Added README

--- Vision/CreateReport/include/ReportList.hpp
+++ Vision/CreateReport/include/ReportList.hpp
@@ -1,3 +1,32 @@
+//******************************************************************************
+//
+//                 Low Cost Vision
+//
+//******************************************************************************
+// Project:        CreateReport
+// File:           ReportList.hpp
+// Description:    This class is the most basic ReportField containing a matrix of values.
+// Author:         Wouter Langerak
+// Notes:          
+//
+// License:        GNU GPL v3
+//
+// This file is part of CreateReport.
+//
+// CreateReport is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// CreateReport is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with CreateReport.  If not, see <http://www.gnu.org/licenses/>.
+//******************************************************************************
+
 #pragma once
 
 #include <string>
