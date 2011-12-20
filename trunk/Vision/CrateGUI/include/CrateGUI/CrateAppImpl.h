314813a8d781886bd31f67171a3848503fa12c24
glenn.meerstra@gmail.com <glenn.meerstra@gmail.com@4ab6f9c5-c1da-f840-b2ac-8f7b3113cc1e>
2011-12-20 10:21:35 +0000

    CrateGUI: Added headers to the .h and .cpp files Changed to the new make file

--- trunk/Vision/CrateGUI/include/CrateGUI/CrateAppImpl.h
+++ trunk/Vision/CrateGUI/include/CrateGUI/CrateAppImpl.h
@@ -1,3 +1,31 @@
+//******************************************************************************
+//
+//                 Low Cost Vision
+//
+//******************************************************************************
+// Project:        CrateGUI
+// File:           CrateAppImpl.h
+// Description:    The implementation of the functions in the GUI
+// Author:         Glenn Meerstra
+// Notes:          ...
+//
+// License:        GNU GPL v3
+//
+// This file is part of CrateGUI.
+//
+// CrateGUI is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// CrateGUI is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with CrateGUI.  If not, see <http://www.gnu.org/licenses/>.
+//******************************************************************************
 #pragma once
 
 #include "CrateGUI/CrateApp.h"
--- trunk/Vision/CrateGUI/src/Main.cpp
+++ trunk/Vision/CrateGUI/src/Main.cpp
@@ -1,3 +1,44 @@
+//******************************************************************************
+//
+//                 Low Cost Vision
+//
+//******************************************************************************
+// Project:        CrateGUI
+// File:           Main.cpp
+// Description:    The main that starts the GUI
+// Author:         Glenn Meerstra
+// Notes:          This program indexes images located in a 'Images' directory next to
+//		   this directory there needs to be a .txt file which contains:
+//		   -backgroundTypes 
+//		   	Black
+//			White
+//			etc.
+//		   -LightOptions
+//			TL light
+//			No extra light
+//			etc.
+//		  -PerspectiveOptions
+//			2D
+//			3D
+//			etc.
+//
+// License:        GNU GPL v3
+//
+// This file is part of CrateGUI.
+//
+// CrateGUI is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// CrateGUI is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with CrateGUI.  If not, see <http://www.gnu.org/licenses/>.
+//******************************************************************************
 #include <wx/wx.h>
 #include <CrateGUI/ChoiceFrameImpl.h>
 #include <CrateGUI/CrateAppImpl.h>
