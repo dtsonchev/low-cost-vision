1efd9bff9b3909d9228b19854de51907dbf07bb7
zep.mouris <zep.mouris@gmail.com>
2011-12-20 10:00:55 +0000

    add the header and the read me

--- Vision/unicap_cv_bridge_test/src/example_main.cpp
+++ Vision/unicap_cv_bridge_test/src/example_main.cpp
@@ -1,3 +1,31 @@
+//******************************************************************************
+//
+//                 Low Cost Vision
+//
+//******************************************************************************
+// Project:        unicap_cv_bridge_test
+// File:           example_main.cpp
+// Description:    Test main for unicap bridge camera
+// Author:         Kasper van nieuwland & Zep Mouris
+// Notes:          for information on sharepoint site: Low Cost Vision > Gedeelde documenten > Technisch > Taak resultaten > Vision > 5 camera cv bridge  
+//
+// License:        GNU GPL v3
+//
+// This file is part of unicap_cv_bridge_test.
+//
+// unicap_cv_bridge_test is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// unicap_cv_bridge_test is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with unicap_cv_bridge_test.  If not, see <http://www.gnu.org/licenses/>.
+//******************************************************************************
 #include "unicap_cv_bridge.hpp"
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
