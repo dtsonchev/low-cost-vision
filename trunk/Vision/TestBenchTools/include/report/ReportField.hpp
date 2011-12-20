7da3187b1d9893b5e396b334e3de5113478cb19e
wouter.langerak@gmail.com <wouter.langerak@gmail.com@4ab6f9c5-c1da-f840-b2ac-8f7b3113cc1e>
2011-12-20 16:15:31 +0000

    CreateReport      *    updated makefile TestBechtools     *    updated makefile     *    added file headers to almost files     *    added TODO.txt ueyeOpencv     *    updated Makefile

--- trunk/Vision/TestBenchTools/include/report/ReportField.hpp
+++ trunk/Vision/TestBenchTools/include/report/ReportField.hpp
@@ -1,3 +1,32 @@
+//******************************************************************************
+//
+//                 Low Cost Vision
+//
+//******************************************************************************
+// Project:        TestBenchTools
+// File:           ReportField.hpp
+// Description:    Base class of the objects available to reports.
+// Author:         Wouter Langerak
+// Notes:          
+//
+// License:        GNU GPL v3
+//
+// This file is part of TestBenchTools.
+//
+// TestBenchTools is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// TestBenchTools is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with TestBenchTools.  If not, see <http://www.gnu.org/licenses/>.
+//******************************************************************************
+
 #pragma once
 #include <string>
 #include <vector>
