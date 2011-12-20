99f1da24467110407d71b2d826c05b9b0aefada2
glenn.meerstra <glenn.meerstra@gmail.com>
2011-12-20 10:54:52 +0000

    MetaDataApp: Added headers to .cpp and .h files Added README.TXT

--- Vision/MetaDataApp/include/MetaDataApp/ChoiceFrame.h
+++ Vision/MetaDataApp/include/MetaDataApp/ChoiceFrame.h
@@ -1,3 +1,31 @@
+//******************************************************************************
+//
+//                 Low Cost Vision
+//
+//******************************************************************************
+// Project:        MetaDataApp
+// File:           ChoiceFrame.h
+// Description:    The implementation of the functions in the GUI
+// Author:         Glenn Meerstra
+// Notes:          ...
+//
+// License:        GNU GPL v3
+//
+// This file is part of MetaDataApp.
+//
+// MetaDataApp is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// MetaDataApp is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with MetaDataApp.  If not, see <http://www.gnu.org/licenses/>.
+//******************************************************************************
 #ifndef __CHOICEFRAME__
 #define __CHOICEFRAME__
 
--- Vision/MetaDataApp/include/MetaDataApp/CrateGUI.h
+++ Vision/MetaDataApp/include/MetaDataApp/CrateGUI.h
@@ -1,9 +1,32 @@
-/*
- * CrateGUI.h
- *
- *  Created on: Nov 1, 2011
- *      Author: glenn
- */
+//******************************************************************************
+//
+//                 Low Cost Vision
+//
+//******************************************************************************
+// Project:        MetaDataApp
+// File:           CrateGUI.h
+// Description:    The implementation of the functions in the GUI
+// Author:         Glenn Meerstra
+// Notes:          ...
+//
+// License:        GNU GPL v3
+//
+// This file is part of MetaDataApp.
+//
+// MetaDataApp is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// MetaDataApp is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with MetaDataApp.  If not, see <http://www.gnu.org/licenses/>.
+//******************************************************************************
+
 
 #ifndef CRATEGUI_H_
 #define CRATEGUI_H_
