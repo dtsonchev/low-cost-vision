923d36deaa11a0bbf1cd245881ff34f915671a7f
zep.mouris@gmail.com <zep.mouris@gmail.com@4ab6f9c5-c1da-f840-b2ac-8f7b3113cc1e>
2011-12-20 09:25:02 +0000

    update the Makefile and add the headers

--- trunk/Deltarobot/gripper/src/gripper.cpp
+++ trunk/Deltarobot/gripper/src/gripper.cpp
@@ -1,3 +1,32 @@
+//******************************************************************************
+//
+//                 Low Cost Vision
+//
+//******************************************************************************
+// Project:        Gripper
+// File:           gripper.cpp
+// Description:    Library for controlling the gripper
+// Author:         Kasper van nieuwland & Zep Mouris
+// Notes:          ...
+//
+// License:        GNU GPL v3
+//
+// This file is part of Gripper.
+//
+// Gripper is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Gripper is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with Gripper.  If not, see <http://www.gnu.org/licenses/>.
+//******************************************************************************
+
 #include <gripper/gripper.h>
 #include <cerrno>
 #include <stdexcept>
