d27b63b54dca2009dccc328dc67bb6a0c3500c06
Jules.Blok@gmail.com <Jules.Blok@gmail.com@4ab6f9c5-c1da-f840-b2ac-8f7b3113cc1e>
2011-12-20 15:45:59 +0000

    CrateDemo * Added headers and README

--- trunk/Vision/CrateDemo/src/main.cpp
+++ trunk/Vision/CrateDemo/src/main.cpp
@@ -1,3 +1,32 @@
+//******************************************************************************
+//
+//                 Low Cost Vision
+//
+//******************************************************************************
+// Project:        CrateDemo
+// File:           main.cpp
+// Description:    The pick-and-place productcrate vision demo
+// Author:         Jules Blok
+// Notes:          None
+//
+// License:        GNU GPL v3
+//
+// This file is part of CrateDemo.
+//
+// CrateDemo is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// CrateDemo is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with CrateDemo.  If not, see <http://www.gnu.org/licenses/>.
+//******************************************************************************
+
 #include "unicap_cv_bridge.hpp"
 #include "FiducialDetector.h"
 #include "CrateDetector.h"
