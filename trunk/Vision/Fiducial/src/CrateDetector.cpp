4a4061ecc0e1de386bca52891ef0ca392647f798
jules.blok@gmail.com <jules.blok@gmail.com@4ab6f9c5-c1da-f840-b2ac-8f7b3113cc1e>
2011-12-20 15:00:57 +0000

    Fiducial * Added headers

--- trunk/Vision/Fiducial/src/CrateDetector.cpp
+++ trunk/Vision/Fiducial/src/CrateDetector.cpp
@@ -1,9 +1,31 @@
-/*
- * CrateDetector.cpp
- *
- *  Created on: Nov 11, 2011
- *      Author: Jules Blok
- */
+//******************************************************************************
+//
+//                 Low Cost Vision
+//
+//******************************************************************************
+// Project:        Fiducial
+// File:           CrateDetector.cpp
+// Description:    Segments point cloud into crates
+// Author:         Jules Blok
+// Notes:          None
+//
+// License:        GNU GPL v3
+//
+// This file is part of Fiducial.
+//
+// Fiducial is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Fiducial is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Fiducial.  If not, see <http://www.gnu.org/licenses/>.
+//******************************************************************************
 
 #include "CrateDetector.h"
 #include "Crate.h"
