aa1dd63169d268b36429f4f86e0141a2bb0496ca
Jules.Blok <jules.blok@gmail.com>
2011-12-20 16:09:39 +0000

    BOWClassifier * Added headers and README

--- Vision/BOWClassifier/include/BOWClassifier/BOWNeuralClassifier.h
+++ Vision/BOWClassifier/include/BOWClassifier/BOWNeuralClassifier.h
@@ -1,9 +1,31 @@
-/*
- * BoWNeuralClassifier.h
- *
- *  Created on: Oct 18, 2011
- *      Author: Jules Blok
- */
+//******************************************************************************
+//
+//                 Low Cost Vision
+//
+//******************************************************************************
+// Project:        BOWClassifier
+// File:           BOWNeuralClassifier.h
+// Description:    A Bag of Words KeyPoint classifier utilizing Neural Networks
+// Author:         Jules Blok
+// Notes:          None
+//
+// License:        GNU GPL v3
+//
+// This file is part of BOWClassifier.
+//
+// BOWClassifier is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// BOWClassifier is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with BOWClassifier.  If not, see <http://www.gnu.org/licenses/>.
+//******************************************************************************
 
 #ifndef BOWNEURALCLASSIFIER_H_
 #define BOWNEURALCLASSIFIER_H_
--- Vision/BOWClassifier/src/BOWClassifier.cpp
+++ Vision/BOWClassifier/src/BOWClassifier.cpp
@@ -1,3 +1,32 @@
+//******************************************************************************
+//
+//                 Low Cost Vision
+//
+//******************************************************************************
+// Project:        BOWClassifier
+// File:           BOWClassifier.h
+// Description:    A Bag of Words KeyPoint classifier
+// Author:         Jules Blok
+// Notes:          None
+//
+// License:        GNU GPL v3
+//
+// This file is part of BOWClassifier.
+//
+// BOWClassifier is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// BOWClassifier is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with BOWClassifier.  If not, see <http://www.gnu.org/licenses/>.
+//******************************************************************************
+
 #include <BOWClassifier/BOWClassifier.h>
 #include <opencv2/core/core.hpp>
 #include <opencv2/features2d/features2d.hpp>
