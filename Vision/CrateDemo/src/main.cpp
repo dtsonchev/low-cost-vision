d3f6e400613de98b4dab6416179e2ea6db43a843
glenn.meerstra <glenn.meerstra@gmail.com>
2011-12-20 11:46:47 +0000

    CrateDemo: lens_correction dependencie changed

--- Vision/CrateDemo/src/main.cpp
+++ Vision/CrateDemo/src/main.cpp
@@ -2,8 +2,8 @@
 #include "FiducialDetector.h"
 #include "CrateDetector.h"
 #include "Crate.h"
-#include "BarcodeDetector.h"
-#include "RectifyImage.h"
+#include "DetectQRCode/BarcodeDetector.h"
+#include "CameraCalibration/RectifyImage.h"
 #include <opencv2/core/core.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
