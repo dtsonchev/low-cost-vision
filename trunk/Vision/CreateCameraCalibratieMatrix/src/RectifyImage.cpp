9a52276d82f350d24426319c876b10a8ae106a8f
glenn.meerstra@gmail.com <glenn.meerstra@gmail.com@4ab6f9c5-c1da-f840-b2ac-8f7b3113cc1e>
2011-12-20 12:00:08 +0000

    CreateCameraCalibratieMatrix: Added headers in .h and .cpp files Updated Makefile MetaDataApp: Removed bin directory Updated Makefile lens_correction: deleted project CrateGUI: updated Makefile

--- trunk/Vision/CreateCameraCalibratieMatrix/src/RectifyImage.cpp
+++ trunk/Vision/CreateCameraCalibratieMatrix/src/RectifyImage.cpp
@@ -1,3 +1,31 @@
+//******************************************************************************
+//
+//                 Low Cost Vision
+//
+//******************************************************************************
+// Project:        MetaDataApp
+// File:           RectifyImage.cpp
+// Description:    The library to rectify images
+// Author:         Glenn Meerstra & Zep Mouris
+// Notes:          ...
+//
+// License:        GNU GPL v3
+//
+// This file is part of MetaDataApp.
+//
+// MetaDataApp is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// MetaDataApp is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with MetaDataApp.  If not, see <http://www.gnu.org/licenses/>.
+//******************************************************************************
 #include <CameraCalibration/RectifyImage.h>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
