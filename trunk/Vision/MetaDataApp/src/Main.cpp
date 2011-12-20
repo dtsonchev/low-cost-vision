098d7a62d77b8b23d927c45e3757c84abbcf5c1c
glenn.meerstra@gmail.com <glenn.meerstra@gmail.com@4ab6f9c5-c1da-f840-b2ac-8f7b3113cc1e>
2011-12-20 10:54:52 +0000

    MetaDataApp: Added headers to .cpp and .h files Added README.TXT

--- trunk/Vision/MetaDataApp/src/Main.cpp
+++ trunk/Vision/MetaDataApp/src/Main.cpp
@@ -1,3 +1,45 @@
+//******************************************************************************
+//
+//                 Low Cost Vision
+//
+//******************************************************************************
+// Project:        MetaDataApp
+// File:           Main.cpp
+// Description:    This program indexes images located in a 'Images' directory next to 
+// 			this directory there needs to be a .txt file which contains: 
+// 			-backgroundTypes 
+//  				Black 
+//  				White 
+//  				etc. 
+//  			-LightOptions 
+//  				TL light 
+//  				No extra light 
+//  				etc. 
+//  			-PerspectiveOptions 
+//  				2D
+//	  			3D 
+//  				etc.
+//
+// Author:         Glenn Meerstra
+// Notes:          ...
+//
+// License:        GNU GPL v3
+//
+// This file is part of MetaDataApp.
+//
+// MetaDataApp is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// MetaDataApp is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with MetaDataApp.  If not, see <http://www.gnu.org/licenses/>.
+//******************************************************************************
 #include <wx/wx.h>
 #include <MetaDataApp/ChoiceFrame.h>
 #include <MetaDataApp/ConverterGUI.h>
