e12b94b0d687b09031e3d67286c815867fbf4411
zep.mouris <zep.mouris@gmail.com>
2011-12-20 15:18:59 +0000

    update the makefile and added the header

--- Vision/FGBGSeperation_test/src/main.cpp
+++ Vision/FGBGSeperation_test/src/main.cpp
@@ -1,3 +1,32 @@
+//******************************************************************************
+//
+//                 Low Cost Vision
+//
+//******************************************************************************
+// Project:        FGBGSeparation_test
+// File:           Main.h
+// Description:    Program that traines with images.Normal images are used and FGBG images where white means forground and black means background. After training the program can generate the FGBG images self.
+// Author:         Franc Pape & Zep Mouris
+// Notes:          ...
+//
+// License:        GNU GPL v3
+//
+// This file is part of FGBGSeparation_test.
+//
+// FGBGSeparation_test is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// FGBGSeparation_test is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with FGBGSeparation_test.  If not, see <http://www.gnu.org/licenses/>.
+//******************************************************************************
+
 #include <cstdlib>
 #include <cassert>
 #include <string>
