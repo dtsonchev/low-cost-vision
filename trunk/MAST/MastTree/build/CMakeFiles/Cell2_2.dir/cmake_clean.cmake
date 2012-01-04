dd740ce48239f0897ac1aa99668a42d41aa8cf0b
francpape@gmail.com <francpape@gmail.com@4ab6f9c5-c1da-f840-b2ac-8f7b3113cc1e>
2012-01-04 10:52:58 +0000

    MastTree:  - Readded to SVN (was deleted with SVN reset)

---
+++ trunk/MAST/MastTree/build/CMakeFiles/Cell2_2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/Cell2_2.dir/src/Cell2_2.o"
+  "../bin/Cell2_2.pdb"
+  "../bin/Cell2_2"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/Cell2_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
