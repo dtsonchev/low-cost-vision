57e70265be764acc4222c7df1863709630b9a7a8
lukas.vermond@gmail.com <lukas.vermond@gmail.com@4ab6f9c5-c1da-f840-b2ac-8f7b3113cc1e>
2012-01-06 16:09:31 +0000

    fixed misc errors in cratedemo & datatypes

--- trunk/Demo/cratedemo/src/Crate.cpp
+++ trunk/Demo/cratedemo/src/Crate.cpp
@@ -1,48 +1,56 @@
 #include <cratedemo/Crate.hpp>
 #include <cratedemo/CrateExceptions.hpp>
 #include <string>
-namespace cratedemo {
-Crate::Crate(std::string name,
-		datatypes::point2f position,
-		float angle,
-		datatypes::size3f size,
-		size_t maxNumberOfObjects):
-			name(name),
-			position(position),
-			angle(angle),
-			size(size),
-			maxNumberOfObjects(maxNumberOfObjects) {
+
+namespace cratedemo
+{
+Crate::Crate(std::string name, datatypes::point2f position, float angle,
+		datatypes::size3f size, size_t maxNumberOfObjects) :
+		name(name), position(position), angle(angle), size(size), maxNumberOfObjects(
+				maxNumberOfObjects)
+{
 	data = new CrateContent*[maxNumberOfObjects];
 	memset(data, NULL, maxNumberOfObjects * sizeof(CrateContent*));
 }
 
-Crate::~Crate() {
+Crate::~Crate()
+{
 	delete[] data;
 }
 
-void Crate::put(size_t index, CrateContent* crateContent) {
-	if(data[index] != NULL) {
+void Crate::put(size_t index, CrateContent* crateContent)
+{
+	if (data[index] != NULL)
+	{
 		throw cratedemo::LocationIsFullException();
 	}
 	data[index] = crateContent;
 }
 
-CrateContent* Crate::get(size_t index) const {
+CrateContent* Crate::get(size_t index) const
+{
 	return data[index];
 }
 
-void Crate::remove(size_t index) {
-	if(data[index] != NULL) {
+void Crate::remove(size_t index)
+{
+	if (data[index] != NULL)
+	{
 		delete data[index];
 		data[index] = NULL;
-	} else {
+	}
+	else
+	{
 		throw cratedemo::LocationIsEmptyException();
 	}
 }
 
-bool Crate::isEmpty() const {
-	for(size_t i =0; i<= maxNumberOfObjects; i++) {
-		if(data[i] != NULL) {
+bool Crate::isEmpty() const
+{
+	for (size_t i = 0; i <= maxNumberOfObjects; i++)
+	{
+		if (data[i] != NULL)
+		{
 			return false;
 		}
 	}
--- trunk/Demo/cratedemo/src/GridCrate.cpp
+++ trunk/Demo/cratedemo/src/GridCrate.cpp
@@ -1,5 +1,6 @@
 #include <cratedemo/GridCrate.hpp>
 #include <cratedemo/Crate.hpp>
+#include <cratedemo/CrateExceptions.hpp>
 #include <datatypes/point2.hpp>
 #include <datatypes/point3.hpp>
 #include <datatypes/size3.hpp>
@@ -21,23 +22,18 @@ GridCrate::GridCrate(std::string name, datatypes::point2f position, float angle,
 datatypes::point3f GridCrate::getCrateContentGripLocation(size_t index) {
 	using datatypes::point2f;
 	using datatypes::point3f;
-	point3f location;
-	location.x = 0;
-	location.y = 0;
-	location.z = 0;
+
 	if (data[index] == NULL) {
 		throw cratedemo::LocationIsEmptyException();
-
-	} else {
-
-		point2f location2D;
-		location2D.x = (-size.width / 2) + (index % gridWidth) * (2 * radiusOfBallContainer + distanceToNext) + distanceToSide + radiusOfBallContainer;
-		location2D.y = (-size.depth / 2) + (index / gridHeight) * (2 * radiusOfBallContainer + distanceToNext) + distanceToSide + radiusOfBallContainer;
-		location2D.rotate(angle);
-		location.x = location2D.x;
-		location.y = location2D.y;
-		location.z = bottomThickness;
-		location += data[index]->gripPoint;
 	}
-	return location;
+
+	point2f location2D;
+	location2D.x = -(size.width / 2) + (index % gridWidth) * (2 * radiusOfBallContainer + distanceToNext) + distanceToSide + radiusOfBallContainer;
+	location2D.y = -(size.depth / 2) + (index / gridHeight) * (2 * radiusOfBallContainer + distanceToNext) + distanceToSide + radiusOfBallContainer;
+	location2D.rotate(angle);
+	point3f location3D(location2D.x, location2D.y, bottomThickness);
+	location3D += data[index]->getGripPoint();
+
+	return location3D;
+}
 }
