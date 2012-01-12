62ada6bce1dc813fdb0ed0f59759dfb4876dc4cf
lukas.vermond <lukas.vermond@gmail.com>
2012-01-12 13:42:30 +0000

    Update to the full working version

--- Demo/vision/src/visionNode.cpp
+++ Demo/vision/src/visionNode.cpp
@@ -34,7 +34,7 @@ visionNode::visionNode(int argc, char* argv[]){
 		int format_number = atoi(argv[2]);
 		cam = new unicap_cv_camera(device_number,format_number);
 		cam->set_auto_white_balance(true);
-		cam->set_exposure(0.022);
+		cam->set_exposure(0.017);
 		camFrame = Mat(cam->get_img_height(), cam->get_img_width(), cam->get_img_format());
 
 		fidDetector = new FiducialDetector();
@@ -43,18 +43,19 @@ visionNode::visionNode(int argc, char* argv[]){
 		qrDetector = new QRCodeDetector();
 
 		point2f::point2fvector rc;
-		rc.push_back(point2f(-54, 113));
 		rc.push_back(point2f(71, 110));
+		rc.push_back(point2f(-54, 113));
 		rc.push_back(point2f(-59, -75));
 		cordTransformer= new pc_rc_transformer(rc,rc);
 
 		rectifier = new RectifyImage();
-		if(!rectifier->initRectify(argv[3], cv::Size(cam->get_img_height(), cam->get_img_width()))){
+		if(!rectifier->initRectify(argv[3], cv::Size( cam->get_img_width(),cam->get_img_height()))){
 			cout << "XML not found" << endl;
 			exit(2);
 		}
+		crateMovementThresshold = 5;
+		crateTracker = new CrateTracker(10 , crateMovementThresshold);
 
-		crateMovementThresshold = 1;
 
 
 
@@ -64,13 +65,43 @@ visionNode::visionNode(int argc, char* argv[]){
 		getAllCratesService = node.advertiseService("getAllCrates", &visionNode::getAllCrates, this);
 }
 
-bool visionNode::getCrate(vision::getCrate::Request &req,vision::getCrate::Request &res)
+bool visionNode::getCrate(vision::getCrate::Request &req,vision::getCrate::Response &res)
 {
+	exCrate crate;
+	bool succeeded = crateTracker->getCrate(req.name, crate);
+	if(succeeded){
+		res.state = crate.getState();
+		vision::CrateMsg msg;
+		msg.name = crate.name;
+		msg.x =crate.rect().center.x;
+		msg.y =crate.rect().center.y;
+		msg.angle = crate.rect().angle;
+		res.crate = msg;
+	} else{
+		res.state = exCrate::state_non_existing;
+		vision::CrateMsg msg;
+		msg.name = "";
+		msg.x =0;
+		msg.y =0;
+		msg.angle = 0;
+		res.crate = msg;
+	}
 	return true;
 }
 
-bool visionNode::getAllCrates(vision::getAllCrates::Request &req,vision::getAllCrates::Request &res)
+bool visionNode::getAllCrates(vision::getAllCrates::Request &req,vision::getAllCrates::Response &res)
 {
+	std::vector<exCrate> allCrates = crateTracker->getAllCrates();
+	for(std::vector<exCrate>::iterator it = allCrates.begin(); it != allCrates.end(); ++it)
+	{
+		res.states.push_back(it->getState());
+		vision::CrateMsg msg;
+		msg.name =it->name;
+		msg.x =it->rect().center.x;
+		msg.y =it->rect().center.y;
+		msg.angle = it->rect().angle;
+		res.crates.push_back(msg);
+	}
 	return true;
 }
 
@@ -80,6 +111,7 @@ visionNode::~visionNode(){
 	delete qrDetector;
 	delete cordTransformer;
 	delete rectifier;
+	delete crateTracker;
 }
 
 void visionNode::printUsage(char* invokeName){
@@ -87,16 +119,14 @@ void visionNode::printUsage(char* invokeName){
 }
 
 void visionNode::run(){
-	crateTracker crateTracker(20 , 5);
 	while(ros::ok()){
 		cam->get_frame(&camFrame);
-
 		rectifier->rectify(camFrame, rectifiedCamFrame);
 		cv::Mat gray;
 		cv::cvtColor(rectifiedCamFrame, gray, CV_BGR2GRAY);
 
 		std::vector<cv::Point2f> fiducialPoints;
-		fidDetector->detect(gray, fiducialPoints);
+		fidDetector->detect(gray, fiducialPoints,&rectifiedCamFrame);
 		if(fiducialPoints.size() == 3){
 			point2f::point2fvector pixelCoordinatesFiducial;
 			Crate::order(fiducialPoints);
@@ -105,13 +135,15 @@ void visionNode::run(){
 			}
 			cordTransformer->set_fiducials_pixel_coordinates(pixelCoordinatesFiducial);
 		} else {
-			ROS_WARN("Number of fiducials not correct needed 3 saw: %d", fiducialPoints.size());
+			//ROS_WARN("Number of fiducials not correct needed 3 saw: %d", fiducialPoints.size());
 		}
 
 		std::vector<Crate> crates;
 		qrDetector->detectCrates(gray, crates);
 		for(std::vector<Crate>::iterator it=crates.begin(); it!=crates.end(); ++it)
 		{
+			it->draw(rectifiedCamFrame);
+
 
 			std::vector<cv::Point2f> points;
 			for(int n = 0; n <3; n++){
@@ -122,9 +154,9 @@ void visionNode::run(){
 			//transfor coordinates
 			it->setPoints(points);
 
-			it->draw(rectifiedCamFrame);
+			ROS_INFO("crate pos: %f %f", it->rect().center.x, it->rect().center.y);
 		}
-		std::vector<CrateEvent> events = crateTracker.update(crates);
+		std::vector<CrateEvent> events = crateTracker->update(crates);
 
 		for(std::vector<CrateEvent>::iterator it = events.begin(); it != events.end(); ++it)
 		{
