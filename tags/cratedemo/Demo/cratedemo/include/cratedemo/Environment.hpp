ae1163eff7886d189c50c502e7449a06c162f576
pascalmuller@gmail.com <pascalmuller@gmail.com@4ab6f9c5-c1da-f840-b2ac-8f7b3113cc1e>
2012-02-16 15:48:04 +0000

    The last team committed an incomplete version that did not compile. Therefore, the tags is meaningless and I'd rather remove it.

--- tags/cratedemo/Demo/cratedemo/include/cratedemo/Environment.hpp
+++
@@ -1,5 +0,0 @@
-#pragma once
-namespace cratedemo {
-	static const float SAFE_HEIGHT = -165.0;
-	static const float TABLE_HEIGHT = -198.0;
-}
--- tags/cratedemo/MAST/MastTreeTestCase/bin/start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo start of script
-rosrun MastTreeTestCase GenericMASTEntity __name:=Entity0 _mastId:=0 _noOfChildren:=4 _currentLevel:=0 &
-echo end of script
